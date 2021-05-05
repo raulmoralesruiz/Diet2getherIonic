a35957a3a4afb32d03b78bcfe97ce238ea3e60b3
Raúl Morales <raulmdaw@gmail.com>
2021-05-05 22:30:27 +0200

    FC

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./pages/entry/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./pages/entry/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/entry/log-in/log-in.module').then( m => m.LogInPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/entry/sign-up/sign-up.module').then( m => m.SignUpPageModule)
   },
 ];
 
