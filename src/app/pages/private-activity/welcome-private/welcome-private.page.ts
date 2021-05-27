1f9fcbf1584142c5628d897e56f383fdafbd2253
Raúl Morales <raulmdaw@gmail.com>
2021-05-27 19:18:48 +0200

    Se crea página private-welcome provisional

--- src/app/pages/private-activity/welcome-private/welcome-private.page.ts
+++ src/app/pages/private-activity/welcome-private/welcome-private.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LogInService } from '../../entry/services/log-in.service';
+import { PrivateService } from '../services/private.service';
 
 @Component({
   selector: 'app-welcome-private',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePrivatePage implements OnInit {
 
-  constructor() { }
+  /* Variable que almacena la actividad privada actual */
+  actualPrivateActivity: any;
+
+  /* Variable que almacena el grupo actual */
+  actualGroup: any;
+
+
+  constructor(private privateService: PrivateService, private login:LogInService) { }
 
   ngOnInit() {
+    this.login.isUserInSession();
+    this.getActivePrivateActivity();
+    this.getActiveGroup();
   }
 
+  /* Método que obtiene la actividad privada actual */
+  getActivePrivateActivity() {
+    this.login.isUserInSession();
+    this.privateService.getPrivateActivity().subscribe(res => {
+      this.actualPrivateActivity = res;
+    });
+  }
+
+  /* Método que obtiene el grupo actual */
+  getActiveGroup() {
+    this.login.isUserInSession();
+    this.privateService.getActiveGroup().subscribe(res => {
+      this.actualGroup = res.actualGroup;
+    });
+  }
+  
 }
